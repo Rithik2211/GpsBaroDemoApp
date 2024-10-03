f279f89789901762cb03e07afa75afa122006b5f
Rithik2211 <rithikpranao22@gmail.com>
2024-10-04 00:37:26 +0530

    Initial Commit

---
+++ GpsBaroDemoApp/GpsBaroDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GpsBaroDemoAppApp.swift
+//  GpsBaroDemoApp
+//
+//  Created by Rithik Pranao on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GpsBaroDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
